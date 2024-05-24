52351dd8398d79e4556136479573abbf2245b2b4
smestry <shashankmestry@gmail.com>
2024-05-24 21:13:26 +0530

    trust status added

--- components/ScoreCard.tsx
+++ components/ScoreCard.tsx
@@ -1,8 +1,27 @@
 import { ITrustScore } from "@/services/score.service"
 import Card from "./ui/Card"
 import ScoreBar from "./ScoreBar"
+import { useEffect, useState } from "react"
+import { cn } from "@/lib/utils"
 
 const ScoreCard = ({ score }: { score: ITrustScore }) => {
+    const [trust, setTrust] = useState("low")
+    const trusts: { [key: string]: string } = {
+        'low': 'bg-red-600',
+        'medium': 'bg-yellow-600',
+        'high': 'bg-green-600'
+    }
+
+    useEffect(() => {
+        if(score.total > 80) {
+            setTrust('high')
+        } else if(score.total > 30) {
+            setTrust('medium')
+        } else {
+            setTrust('low')
+        }
+    }, [score.total])
+
     return (
         <Card>
             <p className="font-medium bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-purple-200 pb-3 text-center">Your DTS Score</p>
@@ -10,9 +29,9 @@ const ScoreCard = ({ score }: { score: ITrustScore }) => {
                 {score.total}
             </div>
             <div className="text-center mb-3">
-                <span className="text-xs bg-red-600 px-2 py-1 rounded-lg uppercase">Low Trust</span>
+                <span className={cn("text-xs font-semibold tracking-wider text-white/75 px-2 py-1 rounded-lg uppercase", trusts[trust])}>{trust} Trust</span>
             </div>
-            <div className="text-xs">{score.address}</div>
+            <div className="text-xs text-center">{score.address}</div>
             <div className="mt-5">
                 <ScoreBar score={score} />
             </div>
