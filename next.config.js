cc3b46e0f9d99b685af5184888ceee43e1cfe124
smestry <shashankmestry@gmail.com>
2024-05-27 16:22:16 +0530

    adding middleware for cors

--- next.config.js
+++ next.config.js
@@ -3,28 +3,7 @@ const nextConfig = {
     webpack: config => {
         config.externals.push('pino-pretty', 'lokijs', 'encoding')
         return config
-    },
-    async headers() {
-        return [
-            {
-                source: "/api/:path*",
-                headers: [
-                    {
-                        key: "Access-Control-Allow-Origin",
-                        value: "*", // Set your origin
-                    },
-                    {
-                        key: "Access-Control-Allow-Methods",
-                        value: "GET, POST, PUT, DELETE, OPTIONS",
-                    },
-                    {
-                        key: "Access-Control-Allow-Headers",
-                        value: "Content-Type, Authorization",
-                    },
-                ],
-            },
-        ];
-    },
+    }
 }
 
 module.exports = nextConfig
