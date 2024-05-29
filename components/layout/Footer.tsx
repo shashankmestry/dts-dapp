37ffa4701cf29fe32176ad7fbd497ec5ec4cf666
smestry <shashankmestry@gmail.com>
2024-05-29 16:58:50 +0530

    updating footer and logo

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <footer>
             <div className="max-w-6xl mx-auto px-4 sm:px-6 py-4 text-center">
-                &copy; DTS | Frantech Services
+                &copy; DTS | Bl0xchain Lab
             </div>
         </footer>
     )
--- components/layout/Logo.jsx
+++ components/layout/Logo.jsx
@@ -2,8 +2,8 @@ import Link from 'next/link'
 
 const Logo = () => {
     return (
-        <Link className="inline-flex text-3xl font-black text-transparent bg-clip-text bg-gradient-to-b from-rose-400 via-purple-600 via-65% to-violet-600" href="/" aria-label="DTS">
-            DTS
+        <Link className="inline-flex text-3xl font-black text-transparent bg-clip-text bg-gradient-to-b from-rose-400 via-purple-600 via-65% to-violet-600 items-baseline" href="/" aria-label="DTS">
+            SPAD <span className='text-base'>DTS</span>
         </Link>
     )
 }
